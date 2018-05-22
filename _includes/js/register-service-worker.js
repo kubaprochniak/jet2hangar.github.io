eebdaaf8c21ab59729a699627a25df7251642084
Jakub Prochniak <Jakub.Prochniak@jet2.com>
2018-05-22 16:48:44 +0100

    + Webbox post structure

--- _includes/js/register-service-worker.js
+++ _includes/js/register-service-worker.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     console.log('CLIENT: service worker registration in progress.');
-    navigator.serviceWorker.register('{{ "assets/js/service-worker.js" | absolute_url }}').then(function() {
+    navigator.serviceWorker.register('/assets/js/{% if page.service-worker %}{{ page.service-worker }}{% else %}service-worker.js{% endif %}').then(function() {
         console.log('CLIENT: service worker registration complete.');
     }, function() {
         console.log('CLIENT: service worker registration failure.');
